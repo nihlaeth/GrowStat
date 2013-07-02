f96aebdd334d4c20c4f830614e580d161b5c4545
Nihlaeth <info@nihlaeth.nl>
2013-07-02 20:36:26 +0200

    Missed an indent

--- growstat/views.py
+++ growstat/views.py
@@ -357,7 +357,7 @@ def height_view(request):
 # (Nihlaeth's old code, split into separate function)
 def make_watering_sql_old(ID):
     sql = 'update timerconfigs set '
-
+    
     sql2 = 'insert into chtc (tcid,t0000,t0030,t0100,t0130,t0200,t0230,t0300,t0330,'
     sql2+='t0400,t0430,t0500,t0530,t0600,t0630,t0700,t0730,t0800,t0830,t0900,t0930,'
     sql2+='t1000,t1030,t1100,t1130,t1200,t1230,t1300,t1330,t1400,t1430,t1500,t1530,'
